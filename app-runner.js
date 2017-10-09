77edf170ec7184ab9539eb5b37ce47a85b232145
boolean99 <boolean99@naver.com>
2017-10-09 16:33:29 +0900

    1009

--- app-runner.js
+++ app-runner.js
@@ -1,5 +1,7 @@
 'use strict';
 
 // app.js 서버 어플리케이션에서 ES6 문법을 사용하기위한 호출파일
-require('babel-register');
-require('./app.js');
+require('babel-register')({
+	presets: ['es2015']
+});
+require('./app');

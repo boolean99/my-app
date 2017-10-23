bfa7e9c5181653f0a750733085183b55cc1bba21
boolean99 <boolean99@naver.com>
2017-10-23 23:32:31 +0900

    enable gzip

--- src/webpack/load-more.js
+++ src/webpack/load-more.js
@@ -1,28 +1,25 @@
-// ES2015 사용을 위한 babel 모듈 호출
-//import 'babel-polyfill';
-
 // 프로젝트 모듈 호출
 import {socketFunc} from './project/socket';
 
-let socket;
+const doc = document;
 
-document.addEventListener('DOMContentLoaded', () => {
+doc.addEventListener('DOMContentLoaded', () => {
   // 돔 로드완료 이벤트
   window.addEventListener('load', () => {
     // 윈도우 로드완료 이벤트
-    
+
     // 소켓 초기화
-    socket = io(location.href);
-    
+    const socket = io(location.href);
+
     socket.on('connect', () => {
       // 소켓에 접속되면 소켓 함수 방출
       socketFunc(socket);
-      
-      document.querySelector('.js-load-more').addEventListener('click', () => {
+
+      doc.querySelector('.js-load-more').addEventListener('click', () => {
         // 포스트 불러오기 관련 서버단 이벤트 호출
         socket.emit('loadMorePostsInServer', {
           id: socket.id,
-          existPostsLength: document.querySelectorAll('.contents-section__item').length
+          existPostsLength: doc.querySelectorAll('.contents-section__item').length
         });
       });
     });
